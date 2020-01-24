831c5f6e1ecd99dbeee6250cd5974e99a958a022
Rainer Eli <claushellsing@gmail.com>
2020-01-24 14:32:47 -0600

    Add House Stories

--- components/House/index.js
+++ components/House/index.js
@@ -1,10 +1,60 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, ActivityIndicator, Text } from 'react-native'
+import styled from 'styled-components/native'
 
-export default function House() {
+const HouseImage = styled.Image`
+    height: 150px;
+    width: 150px;
+`
+
+const FullView = styled.View`
+    flex: 1;
+`
+
+const HouseBanner = styled.View`
+    align-items: center;
+    padding: 5px;
+`
+
+const HouseHeader = styled.View`
+    background-color: 'lightgray';
+`
+
+const HouseName = styled.Text`
+    text-align: center;
+    font-size: 23px;
+    font-weight: bold;
+`
+
+const Words = styled.Text`
+    font-style: italic;
+    font-size: 15px;
+    text-align: center;
+`
+
+export default function House({ image, name, words, currentLord }) {
     return (
-        <View>
-            <Text>New text</Text>
-        </View>
+        <FullView>
+            {image && (
+                <HouseBanner>
+                    <HouseImage
+                        source={{ uri: image }}
+                        PlaceholderContent={<ActivityIndicator />}
+                    />
+                </HouseBanner>
+            )}
+            <HouseHeader>
+                <HouseName>{name}</HouseName>
+                {words && <Words>{words}</Words>}
+            </HouseHeader>
+            <View>
+                {currentLord && (
+                    <View style={{ flex: 1, flexDirection: 'row' }}>
+                        <Text>Lord:</Text>
+                        <Text>{currentLord}</Text>
+                    </View>
+                )}
+            </View>
+        </FullView>
     )
 }
--- components/House/stories.js
+++ components/House/stories.js
@@ -12,8 +12,72 @@ const style = {
 
 const CenteredView = ({ children }) => <View style={style}>{children}</View>
 
+const HouseManderly = {
+    titles: [
+        'Lord of White Harbor',
+        'Warden of the White Knife',
+        'Shield of the Faith',
+        'Defender of the Dispossessed',
+        'Lord Marshal of the Mander',
+        'Knight',
+        'Order of the Green Hand',
+        'Dunstonbury',
+    ],
+    overlords: ['House Stark', 'House Bolton'],
+    ancestralWeapon: [],
+    _id: '5cc08102888dfb00103ccb9c',
+    name: 'House Manderly',
+    image:
+        'https://awoiaf.westeros.org/thumb.php?f=House_Manderly.svg&width=250',
+    coatOfArms:
+        'A white merman with dark green hair, beard and tail, carrying a black trident, over a blue-green field(Aquamarine, a merman argent crined, bearded and queued vert, carrying a trident sable)',
+    words: null,
+    currentLord: 'Wyman Manderly',
+    seat: 'New Castle',
+    region: 'North',
+    founded: null,
+    founder: null,
+    cadetBranch: null,
+    heir: 'Wylis Manderly',
+    isExtinct: false,
+    createdAt: '2019-04-24T15:30:10.309Z',
+    updatedAt: '2019-04-24T15:30:10.309Z',
+    __v: 0,
+}
+
+const HouseStark = {
+    titles: [
+        'King in the North/King of Winter',
+        'Lord of Winterfell',
+        'Warden of the North',
+        'King of the Trident',
+    ],
+    overlords: ["House Baratheon of King's Landing", 'House Bolton'],
+    ancestralWeapon: [],
+    _id: '5cc08102888dfb00103ccba4',
+    name: 'House Stark',
+    image: 'https://awoiaf.westeros.org/thumb.php?f=House_Stark.svg&width=250',
+    coatOfArms:
+        'A running grey direwolf, on an ice-white field(Argent, a direwolf courant cendrée)',
+    words: 'Winter is Coming',
+    currentLord: 'Bran Stark',
+    seat: 'Winterfell',
+    region: 'North',
+    founded: null,
+    founder: 'Bran the Builder',
+    cadetBranch: 'House Greystark',
+    heir: 'Rickon Stark',
+    isExtinct: false,
+    createdAt: '2019-04-24T15:30:10.312Z',
+    updatedAt: '2019-04-24T15:30:10.312Z',
+    __v: 0,
+}
+
 storiesOf('Housse', module)
     .addDecorator(storyFn => <CenteredView>{storyFn()}</CenteredView>)
-    .add('default', () => {
-        return <House></House>
+    .add('Manderly', () => {
+        return <House {...HouseManderly}></House>
+    })
+    .add('Stark', () => {
+        return <House {...HouseStark}></House>
     })
