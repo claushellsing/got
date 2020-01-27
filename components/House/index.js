de86601358154b3cbb0c30a78ec8c99841bd8d58
Rainer Eli <claushellsing@gmail.com>
2020-01-27 01:30:07 -0600

    Basic House Styling

--- components/House/index.js
+++ components/House/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import { View, ActivityIndicator, Text } from 'react-native'
-import styled from 'styled-components/native'
+import { View, ActivityIndicator, Text, FlatList } from 'react-native'
+import { Icon, Image } from 'react-native-elements'
 
-const HouseImage = styled.Image`
-    height: 150px;
-    width: 150px;
-`
+import styled from 'styled-components/native'
 
 const FullView = styled.View`
     flex: 1;
@@ -14,10 +11,6 @@ const FullView = styled.View`
 const HouseBanner = styled.View`
     align-items: center;
     padding: 5px;
-`
-
-const HouseHeader = styled.View`
-    background-color: 'lightgray';
 `
 
 const HouseName = styled.Text`
@@ -32,28 +25,125 @@ const Words = styled.Text`
     text-align: center;
 `
 
-export default function House({ image, name, words, currentLord }) {
+const Titles = styled.View`
+    margin-top: 10px;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Title = styled.View`
+    background-color: rgb(227, 227, 227);
+    border-radius: 50px;
+    padding: 5px 7px;
+    margin: 2px 3px;
+`
+
+const InlineText = styled.View`
+    flex-direction: row;
+    margin: 5px 3px;
+`
+
+const InfoText = styled.Text`
+    font-size: 15px;
+`
+
+const InfoLabel = styled(InfoText)`
+    font-weight: bold;
+    margin-right: 5px;
+`
+
+const OverLordTitle = styled.Text`
+    text-align: center;
+    font-weight: bold;
+    font-size: 15px;
+    margin-top: 10px;
+    margin-bottom: 5px;
+`
+
+export default function House({
+    image,
+    name,
+    words,
+    titles,
+    overlords,
+    currentLord,
+    region,
+    seat,
+}) {
     return (
         <FullView>
             {image && (
                 <HouseBanner>
-                    <HouseImage
+                    <Image
                         source={{ uri: image }}
+                        style={{ width: 150, height: 150 }}
                         PlaceholderContent={<ActivityIndicator />}
                     />
                 </HouseBanner>
             )}
-            <HouseHeader>
+            <View>
                 <HouseName>{name}</HouseName>
                 {words && <Words>{words}</Words>}
-            </HouseHeader>
+            </View>
+            {titles && (
+                <Titles>
+                    {titles.map(title => {
+                        return (
+                            <Title key={title}>
+                                <Text
+                                    style={{ fontSize: 12, color: '#444444' }}
+                                >
+                                    {title}
+                                </Text>
+                            </Title>
+                        )
+                    })}
+                </Titles>
+            )}
             <View>
-                {currentLord && (
-                    <View style={{ flex: 1, flexDirection: 'row' }}>
-                        <Text>Lord:</Text>
-                        <Text>{currentLord}</Text>
-                    </View>
-                )}
+                <View style={{ marginTop: 10 }}>
+                    <InlineText>
+                        <InfoLabel>Current Lord:</InfoLabel>
+                        <InfoText>{currentLord}</InfoText>
+                    </InlineText>
+                </View>
+                <View>
+                    {region && (
+                        <InlineText>
+                            <InfoLabel>Region:</InfoLabel>
+                            <InfoText>{region}</InfoText>
+                        </InlineText>
+                    )}
+                    {seat && (
+                        <InlineText>
+                            <InfoLabel>Seat:</InfoLabel>
+                            <InfoText>{seat}</InfoText>
+                        </InlineText>
+                    )}
+                </View>
+                <View>
+                    <OverLordTitle>Overlords</OverLordTitle>
+                    <FlatList
+                        data={overlords}
+                        keyExtractor={item => item}
+                        renderItem={({ item }) => {
+                            return (
+                                <InlineText style={{ alignItems: 'center' }}>
+                                    <Icon
+                                        size={12}
+                                        name="circle"
+                                        type="font-awesome"
+                                        color="#c7c7c7"
+                                    />
+                                    <Text style={{ marginLeft: 5 }}>
+                                        {item}
+                                    </Text>
+                                </InlineText>
+                            )
+                        }}
+                    />
+                </View>
             </View>
         </FullView>
     )
